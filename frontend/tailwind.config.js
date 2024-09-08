8744d90635f96e0bd9dcdfc7321d0df0de5f1bd9
EmericHrv <emeric.hrv@gmail.com>
2024-09-08 11:13:24 +0200

    Update color variables in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          light: process.env.PRIMARY_LIGHT || '#A493FF', // Couleur dérivée claire
-          DEFAULT: process.env.PRIMARY_DEFAULT || '#773EEF', // Couleur primaire par défaut
-          dark: process.env.PRIMARY_DARK || '#5A2EBF', // Couleur dérivée foncée
+          light: '#fff385', // Couleur dérivée claire
+          DEFAULT: '#ffde00', // Couleur primaire par défaut
+          dark: '#ccaf00', // Couleur dérivée foncée
         },
         text: {
-          DEFAULT: process.env.TEXT_COLOR || '#FFFFFF', // Par défaut blanc, peut être noir
+          DEFAULT: '#000000', // Par défaut blanc, peut être noir
         },
       },
     },
